b08ab6f061b275b0a9b72662a4c87589e36e0c7f
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-09 01:11:25 +0600

    Add basic subscription publishing using Atmosphere broadcast
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- hub/src/main/java/com/smartitengineering/event/hub/EventBroadcaster.java
+++ hub/src/main/java/com/smartitengineering/event/hub/EventBroadcaster.java
@@ -1,10 +1,8 @@
 package com.smartitengineering.event.hub;
 
-import java.util.concurrent.atomic.AtomicBoolean;
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.AtmosphereResourceEvent;
 import org.atmosphere.jersey.JerseyBroadcaster;
-import org.atmosphere.util.LoggerUtils;
 
 /**
  *
@@ -13,8 +11,6 @@ import org.atmosphere.util.LoggerUtils;
 public class EventBroadcaster
     extends JerseyBroadcaster {
 
-  private final AtomicBoolean isSet = new AtomicBoolean(false);
-
   public EventBroadcaster() {
     super();
   }
@@ -22,12 +18,6 @@ public class EventBroadcaster
   @Override
   protected void broadcast(final AtmosphereResource r,
                            final AtmosphereResourceEvent e) {
-    if (!isSet.getAndSet(true)) {
-      LoggerUtils.getLogger().info("This is just an example that demonstrate " +
-                                   "how a Broadcaster can be customized using atmosphere.xml or by " +
-                                   "defining it inside web.xml");
-    }
-
     super.broadcast(r, e);
   }
 }
--- hub/src/main/java/com/smartitengineering/event/hub/HubResource.java
+++ hub/src/main/java/com/smartitengineering/event/hub/HubResource.java
@@ -3,32 +3,31 @@ package com.smartitengineering.event.hub;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.PathParam;
 import org.atmosphere.annotation.Broadcast;
 import org.atmosphere.annotation.Suspend;
 import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.jersey.Broadcastable;
 
 /**
  *
  * @author imyousuf
  */
-@Path("/")
+@Path("/{channel}")
 public class HubResource {
 
-  @Context private Broadcaster broadcaster;
+  @PathParam("channel")
+  private Broadcaster broadcaster;
 
   @GET
-  @Suspend(outputComments=false)
-  public Response register() {
-    return Response.noContent().build();
+  @Suspend(outputComments = false)
+  public Broadcastable register() {
+    return new Broadcastable(broadcaster);
   }
 
-  @Broadcast
+  @Broadcast(resumeOnBroadcast=true)
   @POST
-  public Response broadcast() {
-    return Response.ok("Hello!", MediaType.TEXT_PLAIN).build();
+  public Broadcastable broadcast() {
+    return new Broadcastable("Hello!", broadcaster);
   }
-
 }
