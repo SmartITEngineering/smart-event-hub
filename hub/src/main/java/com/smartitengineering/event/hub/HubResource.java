41095b0f749831dd111625ba50e8cff920f3d746
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-09 08:56:44 +0600

    Get a simple connected Pub-Sub working
    Combination of no resumeOnBroadcast and @Schedule ensures that
    events every 10 seconds will be buffered and flushed at @Schedule's
    'period'.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- hub/src/main/java/com/smartitengineering/event/hub/HubResource.java
+++ hub/src/main/java/com/smartitengineering/event/hub/HubResource.java
@@ -1,10 +1,14 @@
 package com.smartitengineering.event.hub;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 import org.atmosphere.annotation.Broadcast;
+import org.atmosphere.annotation.Schedule;
 import org.atmosphere.annotation.Suspend;
 import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.jersey.Broadcastable;
@@ -21,13 +25,22 @@ public class HubResource {
 
   @GET
   @Suspend(outputComments = false)
+  @Produces
   public Broadcastable register() {
     return new Broadcastable(broadcaster);
   }
 
-  @Broadcast(resumeOnBroadcast=true)
+  @Schedule(period=10)
   @POST
-  public Broadcastable broadcast() {
-    return new Broadcastable("Hello!", broadcaster);
+  @Path("/ping")
+  public Response ping() {
+    return Response.noContent().build();
+  }
+
+  @Broadcast
+  @POST
+  @Consumes
+  public Broadcastable broadcast(String message) {
+    return new Broadcastable(message, broadcaster);
   }
 }
