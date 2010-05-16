8771dc23d03f2e988cf7217b564130ca953ed1dd
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-16 21:16:55 +0600

    Add SPI and SPI DB implementation module structure
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ hub-spi/src/main/java/com/smartitengineering/event/hub/spi/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.event.hub.spi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
