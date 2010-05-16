ef425e1d38c5a0165b2cc68ef5e553ed272a2343
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-16 21:06:56 +0600

    Add integration test setup for hub-core
    Use -DskipTests=true to skip the tests and integration tests. Note that it
    will not skip other steps in the lifecycle, e.g. pre/post-integration-test
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ hub-core/src/test/java/com/smartitengineering/event/hub/core/HubResourceITCase.java
@@ -0,0 +1,39 @@
+/*
+It is a application for event distribution to event n-consumers with m-sources.
+Copyright (C) 2010 "Imran M Yousuf <imran@smartitengineering.com>"
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.event.hub.core;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author imyousuf
+ */
+public class HubResourceITCase
+    extends TestCase {
+
+  @Override
+  protected void setUp()
+      throws Exception {
+    System.out.println("::::::::::: setUp :::::::::::");
+    super.setUp();
+  }
+
+  public void testHubResource() {
+    System.out.println("::::::::::: testHubResource :::::::::::");
+  }
+}
