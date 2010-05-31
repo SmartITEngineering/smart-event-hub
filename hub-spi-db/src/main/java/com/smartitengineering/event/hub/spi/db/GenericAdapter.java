ad78c460023198508f354a2a3d9867344a45b0e9
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-05-31 21:11:50 +0600

    Add a generic adapter API and implementation
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

---
+++ hub-spi-db/src/main/java/com/smartitengineering/event/hub/spi/db/GenericAdapter.java
@@ -0,0 +1,41 @@
+/*
+It is a application for event distribution to event n-consumers with m-sources.
+Copyright (C) 2010 "Imran M Yousuf <imran@smartitengineering.com>"
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.smartitengineering.event.hub.spi.db;
+
+import com.smartitengineering.domain.PersistentDTO;
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ *
+ * @author imyousuf
+ */
+public interface GenericAdapter<F, T extends PersistentDTO> {
+
+  public T convert(F fromBean);
+
+  public Collection<T> convert(F... fromBeans);
+
+  public void merge(Map.Entry<F, T> bean);
+
+  public void merge(Collection<Map.Entry<F, T>> beans);
+
+  public F convertInversely(T toBean);
+
+  public Collection<F> convertInversely(T... toBeans);
+}
