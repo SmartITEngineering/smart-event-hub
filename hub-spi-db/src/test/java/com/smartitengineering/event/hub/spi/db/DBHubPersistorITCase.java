221fecf987196bab0f4a8799f6b84e442ac4c33a
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-06-02 20:16:35 +0600

    Add channel creation test case
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- hub-spi-db/src/test/java/com/smartitengineering/event/hub/spi/db/DBHubPersistorITCase.java
+++ hub-spi-db/src/test/java/com/smartitengineering/event/hub/spi/db/DBHubPersistorITCase.java
@@ -17,7 +17,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.smartitengineering.event.hub.spi.db;
 
+import com.smartitengineering.event.hub.api.Channel;
+import com.smartitengineering.event.hub.api.Filter;
+import com.smartitengineering.event.hub.api.Filter.SupportedMimeType;
+import com.smartitengineering.event.hub.api.impl.APIFactory;
+import com.smartitengineering.event.hub.spi.HubPersistentStorer;
 import com.smartitengineering.event.hub.spi.HubPersistentStorerSPI;
+import java.util.Date;
 import junit.framework.TestCase;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -29,10 +35,57 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class DBHubPersistorITCase
     extends TestCase {
 
+  private static ApplicationContext applicationContext;
+
+  @Override
+  protected void setUp()
+      throws Exception {
+    super.setUp();
+    if (applicationContext == null) {
+      applicationContext = new ClassPathXmlApplicationContext(
+          "com/smartitengineering/event/hub/spi/db/app-max-context.xml");
+    }
+  }
+
   public void testSPIInjection() {
-    ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
-        "com/smartitengineering/event/hub/spi/db/app-max-context.xml");
     assertNotNull(applicationContext);
     assertNotNull(HubPersistentStorerSPI.getInstance().getStorer());
+  }
+
+  public void testCreateChannel() {
+    final String someName = "someName";
+    Channel channel = APIFactory.getChannelBuilder(someName).build();
+    final HubPersistentStorer storer =
+                              HubPersistentStorerSPI.getInstance().getStorer();
+    assertNotNull(storer);
+    assertEquals(someName.toLowerCase(), channel.getName());
+    storer.create(channel);
+    channel = storer.getChannel(someName);
+    assertNotNull(channel);
+    assertNotNull(channel.getName());
+    assertEquals(someName.toLowerCase(), channel.getName());
+    assertNotNull(channel.getCreationDateTime());
+    assertNull(channel.getAuthToken());
+    assertNull(channel.getAutoExpiryDateTime());
+    assertNull(channel.getDescription());
+    assertNull(channel.getFilter());
+    String desc = "Desc", authToken = "auth_token", script = "script",
+        name = "name";
+    Date expiry = new Date();
+    final SupportedMimeType mime = SupportedMimeType.JYTHON;
+    Filter filter = APIFactory.getFilter( mime, script);
+    channel = APIFactory.getChannelBuilder(name).filter(filter).description(
+        desc).authToken(authToken).autoExpiryDateTime(expiry).build();
+    storer.create(channel);
+    channel = storer.getChannel(name);
+    assertNotNull(channel);
+    assertNotNull(channel.getName());
+    assertEquals(name, channel.getName());
+    assertNotNull(channel.getCreationDateTime());
+    assertEquals(authToken, channel.getAuthToken());
+    assertEquals(expiry, channel.getAutoExpiryDateTime());
+    assertEquals(desc, channel.getDescription());
+    assertEquals(mime, channel.getFilter().getMimeType());
+    assertEquals(script, channel.getFilter().getFilterScript());
   }
 }
