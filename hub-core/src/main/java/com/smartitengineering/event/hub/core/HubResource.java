255c183cc6d2b5ca1e59ddcc3b18a8087e485916
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-06-07 21:56:37 +0600

    Add different HTTP request implementation
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- hub-core/src/main/java/com/smartitengineering/event/hub/core/HubResource.java
+++ hub-core/src/main/java/com/smartitengineering/event/hub/core/HubResource.java
@@ -18,14 +18,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 package com.smartitengineering.event.hub.core;
 
 import com.smartitengineering.event.hub.api.Channel;
+import com.smartitengineering.event.hub.api.impl.APIFactory;
+import com.smartitengineering.event.hub.spi.HubPersistentStorer;
 import com.smartitengineering.event.hub.spi.HubPersistentStorerSPI;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
@@ -48,15 +52,27 @@ public class HubResource {
 
   @PUT
   @Consumes(MediaType.APPLICATION_JSON)
-  public Response create(Channel channel) {
+  public Response put(Channel channel) {
     Response response;
     try {
-      HubPersistentStorerSPI.getInstance().getStorer().create(channel);
+      if (!channelName.equals(channel.getName())) {
+        throw new IllegalArgumentException("Names do not match!");
+      }
+      HubPersistentStorer storer = HubPersistentStorerSPI.getInstance().
+          getStorer();
+      Channel myChannel = storer.getChannel(channelName);
+      if (myChannel == null) {
+        storer.create(channel);
+      }
+      else {
+        storer.update(APIFactory.getChannelBuilder(channel).creationDateTime(
+            myChannel.getCreationDateTime()).build());
+      }
       response = Response.created(UriBuilder.fromResource(HubResource.class).
           build(channelName)).build();
     }
     catch (Throwable th) {
-      response = Response.status(Response.Status.BAD_REQUEST).entity(th).build();
+      throw new WebApplicationException(th, Response.Status.BAD_REQUEST);
     }
     return response;
   }
@@ -65,13 +81,48 @@ public class HubResource {
   @Suspend(outputComments = false)
   @Produces
   public Broadcastable register() {
+    checkChannelExistence();
     return new Broadcastable(broadcaster);
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("info")
+  public Response getChannelInfo() {
+    Channel channel = HubPersistentStorerSPI.getInstance().getStorer().
+        getChannel(channelName);
+    if (channel != null) {
+      return Response.ok(channel).build();
+    }
+    else {
+      return Response.status(Response.Status.NOT_FOUND).build();
+    }
   }
 
   @Broadcast
   @POST
   @Consumes
   public Broadcastable broadcast(String message) {
+    checkChannelExistence();
     return new Broadcastable(message, broadcaster);
+  }
+
+  @DELETE
+  public Response delete() {
+    Channel channel = checkChannelExistence();
+    HubPersistentStorerSPI.getInstance().getStorer().delete(channel);
+    return Response.ok().build();
+  }
+
+  protected Channel checkChannelExistence()
+      throws WebApplicationException {
+    final HubPersistentStorer storer =
+                              HubPersistentStorerSPI.getInstance().getStorer();
+    Channel channel = storer.getChannel(channelName);
+    if (channel == null) {
+      throw new WebApplicationException(Response.status(
+          Response.Status.NOT_FOUND).build());
+    }
+    return channel;
   }
 }
