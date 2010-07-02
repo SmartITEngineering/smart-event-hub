098221df5e74c8e1a92a8b5a0d83f901ca90fa15
Imran M Yousuf <imyousuf@smartitengineering.com>
2010-07-02 16:37:53 +0600

    Upgrade all dependency versions to latest versions
    In process had to make some small fix to the broadcaster implementation and
    had to add an alternate constructor.
    
    Signed-off-by: Imran M Yousuf <imyousuf@smartitengineering.com>

--- hub-core/src/main/java/com/smartitengineering/event/hub/core/EventBroadcaster.java
+++ hub-core/src/main/java/com/smartitengineering/event/hub/core/EventBroadcaster.java
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 package com.smartitengineering.event.hub.core;
 
 import org.atmosphere.cpr.AtmosphereResource;
@@ -29,6 +29,10 @@ public class EventBroadcaster
     extends JerseyBroadcaster {
 
   public EventBroadcaster() {
+    super();
+  }
+
+  public EventBroadcaster(String channelName) {
     super();
   }
 
