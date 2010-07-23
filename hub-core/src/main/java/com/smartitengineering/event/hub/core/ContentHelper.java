5cf27577c586f737ae5958e01cc1a60b7ebe56e6
kaisar <kaisar@smartitengineering.com>
2010-07-23 18:05:12 +0600

    Add a utility class named ContentHelper
    It converts InputStream type to String
    
    Signed-off-by: kaisar <kaisar@smartitengineering.com>

---
+++ hub-core/src/main/java/com/smartitengineering/event/hub/core/ContentHelper.java
@@ -0,0 +1,65 @@
+/*
+It is a application for event distribution to event n-consumers with m-sources.
+Copyright (C) 2010 "Imran M Yousuf <imran@smartitengineering.com>"
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.smartitengineering.event.hub.core;
+
+import com.smartitengineering.event.hub.api.Content;
+import com.smartitengineering.event.hub.api.Event;
+import java.io.InputStream;
+import java.util.Map;
+import java.util.WeakHashMap;
+import org.apache.commons.io.IOUtils;
+
+/**
+ *
+ * @author kaisar
+ */
+public class ContentHelper {
+
+  private Content content;
+  private InputStream contentStream;
+  private String contentAsString;
+  private final Map<Event, String> contentCache = new WeakHashMap<Event, String>();
+
+  public void setContent(Content content) {
+    this.content = content;
+  }
+  
+  private void convert()
+  {
+    contentStream=(InputStream)content;
+    if(contentStream!=null)
+    {
+      try
+      {
+        contentAsString=IOUtils.toString(contentStream);
+      }
+      catch (Exception ex)
+      {
+
+      }
+    }
+  }
+
+  public String getContentAsString() {
+    convert();
+    return contentAsString;
+  }
+
+
+}
