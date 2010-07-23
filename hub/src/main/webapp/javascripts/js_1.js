aa40fdfecc6127c7e011f066d703668c6ea3ceb5
kaisar <kaisar@smartitengineering.com>
2010-07-23 18:07:26 +0600

    Add comments for license, copyright
    Signed-off-by: kaisar <kaisar@smartitengineering.com>

--- hub/src/main/webapp/javascripts/js_1.js
+++ hub/src/main/webapp/javascripts/js_1.js
@@ -1,8 +1,20 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*
+It is a application for event distribution to event n-consumers with m-sources.
+Copyright (C) 2010 "Imran M Yousuf <imran@smartitengineering.com>"
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later
+version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 function change()
 {
     var className=document.getElementById("div1").className;
